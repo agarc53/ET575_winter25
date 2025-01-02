e4887263f04e24afdef958f47eabd4d30c451a94
agarc53 <aaron.g32482@gmail.com>
2025-01-02 21:31:23 +0000

    Day 1 work

---
+++ lab2_Garcia.cpp
@@ -0,0 +1,44 @@
+/*
+Aaron Garcia
+Lab 2, input and output streaming,string and character variables
+Jan2, 2025
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "\v --------- Example 1: cout and endl -----------" << endl;
+    cout << "\v This is my first program!" << endl;
+    cout << "\nLab 2 \t\t Varables" << endl;
+
+    cout << "\v---------- Example 2: string variables --------" << endl;
+    // declare identifier (variable name)
+    string username;
+    cout << username << endl;
+    cout << "Initial username: " << username << endl;
+    username = "Peter Pan";
+    cout << "Updated username = " << username << endl;
+    cout << "Enter a username: ";
+    cin >> username;
+    cout << "Entered username: " << username << endl;
+
+    cout << "\v---------- Example 3: character variables --------" << endl;
+
+    char symbol = '$';
+    cout << "Initial symbol = " << symbol << endl;
+    cout << "Which is your favorite symbol? ";
+    cin >> symbol;
+    cout << symbol << " is my favorite symbol :)" << endl;
+
+    cout << "\v---------- EXERCISE --------" << endl;
+    string country;
+    char gender;
+
+    cout << "What country are you from? " << endl;
+    cin >> country;
+    cout << "I am from " << country << endl; 
+    cout << "What is your gender,'m' for male, 'f' for female, o for other? " <<  endl;
+    cin >> gender;
+
+    return 0;
+}
